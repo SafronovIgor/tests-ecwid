8a24035b9d67fb0b72c6b47722ecb4d2b004edd4
Safronov Igor <safronov@bestly.ru>
2024-05-19 17:21:36 +0300

    fix: copy collections on input

--- src/CopyUtils.java
+++ src/CopyUtils.java
@@ -1,34 +1,50 @@
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
 import java.util.*;
 
 public final class CopyUtils {
+
     public static void main(String[] args) throws Exception {
         System.out.println("*** Test: 1 ***");
-        ArrayList<String> books = new ArrayList<>();
-        books.add("book 1");
-        var obj = new Man("Alex", 45, books);
-        var copy = CopyUtils.deepClone(obj);
-        System.out.println("Original: " + obj);
-        System.out.println("Cloned: " + copy);
-    }
+        var mans = new ArrayList<>();
+        mans.add(new Man("Alex", 34, List.of("book 1")));
+        mans.add(new Man("Igor", 24, new ArrayList<>(List.of("book 2"))));
+        mans.add(new Man("Sergei", 32, new LinkedList<>(List.of("book 3"))));
 
-    private static final IdentityHashMap<Object, Object> cloneMap = new IdentityHashMap<>();
+        var clone = CopyUtils.deepClone(mans);
+        System.out.println(clone);
+    }
 
     @SuppressWarnings("unchecked")
     public static <T> T deepClone(T original) throws Exception {
-        if (cloneMap.containsKey(original)) {
-            return (T) cloneMap.get(original);
+        Map<Object, Object> cloneMap = new IdentityHashMap<>();
+        return (T) deepCloneInternal(original, cloneMap);
+    }
+
+    private static Object deepCloneInternal(Object original, Map<Object, Object> cloneMap) throws Exception {
+        if (original == null || isImmutable(original)) {
+            return original;
         }
 
-        var copy = (T) createInstanceWithDefaults(original.getClass());
+        if (cloneMap.containsKey(original)) {
+            return cloneMap.get(original);
+        }
+
+        var copy = createInstanceWithDefaults(original.getClass());
         cloneMap.put(original, copy);
 
-        deepCopyFields(copy, original);
+        if (original instanceof Collection) {
+            copy = copyCollection((Collection<?>) original, cloneMap);
+        } else if (original instanceof Map) {
+            copy = copyMap((Map<?, ?>) original, cloneMap);
+        } else {
+            deepCopyFields(copy, original, cloneMap);
+        }
 
         return copy;
     }
 
-    public static Object createInstanceWithDefaults(Class<?> clazz) throws Exception {
+    private static Object createInstanceWithDefaults(Class<?> clazz) throws Exception {
         var constructor = getConstructorForCopy(clazz);
         constructor.setAccessible(true);
         var parameters = getDefaultParameters(constructor);
@@ -48,118 +64,96 @@ public final class CopyUtils {
 
     private static Object[] getDefaultParameters(Constructor<?> constructor) {
         return Arrays.stream(constructor.getParameterTypes())
-                .map(type -> {
-                    if (type.isPrimitive()) {
-                        return switch (type.getName()) {
-                            case "boolean" -> false;
-                            case "byte" -> (byte) 0;
-                            case "char" -> '\0';
-                            case "short" -> (short) 0;
-                            case "int" -> 0;
-                            case "long" -> 0L;
-                            case "float" -> 0.0f;
-                            case "double" -> 0.0;
-                            default -> null;
-                        };
-                    }
-                    return null;
-                })
+                .map(CopyUtils::getDefaultValue)
                 .toArray();
     }
 
-    public static <T> void deepCopyFields(T copy, T original) {
-        var clazz = original.getClass();
+    private static Object getDefaultValue(Class<?> type) {
+        if (type.isPrimitive()) {
+            return switch (type.getName()) {
+                case "boolean" -> false;
+                case "byte" -> (byte) 0;
+                case "char" -> '\0';
+                case "short" -> (short) 0;
+                case "int" -> 0;
+                case "long" -> 0L;
+                case "float" -> 0.0f;
+                case "double" -> 0.0;
+                default -> null;
+            };
+        }
+        return null;
+    }
 
+    private static <T> void deepCopyFields(T copy, T original, Map<Object, Object> cloneMap) {
+        var clazz = original.getClass();
         Arrays.stream(clazz.getDeclaredFields())
-                .peek(field -> field.setAccessible(true))
+                .filter(field -> !field.isSynthetic() && !Modifier.isFinal(field.getModifiers()) && !Modifier.isTransient(field.getModifiers()))
                 .forEach(field -> {
+                    field.setAccessible(true);
                     try {
-                        Object value = field.get(original);
+                        var value = field.get(original);
                         if (value instanceof Collection) {
-                            field.set(copy, copyCollection((Collection<?>) value));
+                            field.set(copy, copyCollection((Collection<?>) value, cloneMap));
                         } else if (value instanceof Map) {
-                            field.set(copy, copyMap((Map<?, ?>) value));
+                            field.set(copy, copyMap((Map<?, ?>) value, cloneMap));
+                        } else if (!isImmutable(value)) {
+                            field.set(copy, deepCloneInternal(value, cloneMap));
                         } else {
-                            if (!(value instanceof String) && !value.getClass().isPrimitive() && isImmutable(value)) {
-                                field.set(copy, deepClone(value));
-                            } else {
-                                field.set(copy, value);
-                            }
+                            field.set(copy, value);
                         }
                     } catch (Exception e) {
                         throw new RuntimeException("Failed to copy field: " + field.getName(), e);
                     }
                 });
-
     }
 
+
     @SuppressWarnings("unchecked")
-    public static <E> Collection<E> copyCollection(Collection<? extends E> original) {
-        if (isImmutableCollection(original)) {
+    private static <E> Collection<E> copyCollection(Collection<? extends E> original, Map<Object, Object> cloneMap) throws Exception {
+        if (isImmutable(original)) {
             return (Collection<E>) original;
         }
-        try {
-            var collectionType = (Class<? extends Collection<E>>) original.getClass();
-            var constructor = collectionType.getDeclaredConstructor();
-            constructor.setAccessible(true);
-            var copy = constructor.newInstance();
+        var collectionType = (Class<? extends Collection<E>>) original.getClass();
+        var constructor = collectionType.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        var copy = constructor.newInstance();
 
-            for (E element : original) {
-                if (element != null && isImmutable(element)) {
-                    element = deepClone(element);
-                }
-                copy.add(element);
-            }
-
-            return copy;
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to copy collection", e);
+        for (E element : original) {
+            copy.add((E) deepCloneInternal(element, cloneMap));
         }
+
+        return copy;
     }
 
     @SuppressWarnings("unchecked")
-    public static <K, V> Map<K, V> copyMap(Map<? extends K, ? extends V> original) {
-        if (isImmutableMap(original)) {
+    private static <K, V> Map<K, V> copyMap(Map<? extends K, ? extends V> original, Map<Object, Object> cloneMap) throws Exception {
+        if (isImmutable(original)) {
             return (Map<K, V>) original;
         }
-        try {
-            var mapType = (Class<? extends Map<K, V>>) original.getClass();
-            var constructor = mapType.getDeclaredConstructor();
-            constructor.setAccessible(true);
-            var copy = constructor.newInstance();
+        var mapType = (Class<? extends Map<K, V>>) original.getClass();
+        var constructor = mapType.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        var copy = constructor.newInstance();
 
-            for (var entry : original.entrySet()) {
-                K key = entry.getKey();
-                V value = entry.getValue();
-
-                if (key != null && isImmutable(key)) {
-                    key = deepClone(key);
-                }
-                if (value != null && isImmutable(value)) {
-                    value = deepClone(value);
-                }
-
-                copy.put(key, value);
-            }
-
-            return copy;
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to copy map", e);
+        for (var entry : original.entrySet()) {
+            K key = (K) deepCloneInternal(entry.getKey(), cloneMap);
+            V value = (V) deepCloneInternal(entry.getValue(), cloneMap);
+            copy.put(key, value);
         }
+
+        return copy;
     }
 
     private static boolean isImmutable(Object obj) {
-        return !(obj instanceof String) &&
-                !(obj instanceof Number) &&
-                !(obj instanceof Boolean) &&
-                !(obj instanceof Character);
-    }
-
-    private static boolean isImmutableCollection(Collection<?> collection) {
-        return collection.getClass().getName().startsWith("java.util.ImmutableCollections$");
-    }
-
-    private static boolean isImmutableMap(Map<?, ?> map) {
-        return map.getClass().getName().startsWith("java.util.ImmutableCollections$");
+        if (obj == null) return true;
+        if (obj instanceof String || obj instanceof Number || obj instanceof Boolean || obj instanceof Character) {
+            return true;
+        }
+        if (obj instanceof Collection || obj instanceof Map) {
+            var className = obj.getClass().getName();
+            return className.contains("Immutable") || className.contains("Unmodifiable");
+        }
+        return false;
     }
 }
